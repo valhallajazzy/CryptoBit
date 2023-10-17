9ef7a1d4a80325dc624443f170f1842595f024d2
HypeBeast18 <dimadeg2202@gmail.com>
2023-10-17 22:36:33 +0500

    Something new

---
+++ client/src/components/ui/contextMenu/ContextMenu.jsx
@@ -0,0 +1,18 @@
+
+import './ContextMenu.scss'
+
+
+const ContextMenu = ({id}) => {
+
+  
+
+  return (
+    <menu>
+      <button onClick={handleClick(e)}>
+
+      </button>
+    </menu>
+  )
+}
+
+export default ContextMenu
