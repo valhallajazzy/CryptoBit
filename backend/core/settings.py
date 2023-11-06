698b750307d8fad08c606f8f8a79a7cea6e93ee5
Dima <dimadeg2202@gmail.com>
2023-11-06 21:17:09 +0500

    Added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nft',
-        'USER': 'vale',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
